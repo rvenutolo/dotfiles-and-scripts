8d9e59da49dc9f6bc8169d3173f1e468432a8025
Rick Venutolo <rick@venutolo.org>
2015-06-22 08:13:21 -0400

    Fixed typo in sync_dotfiles_and_scripts.sh

--- scripts/sync_dotfiles_and_scripts.sh
+++ scripts/sync_dotfiles_and_scripts.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-DOTFILED_TARGET_DIR=~
+DOTFILES_TARGET_DIR=~
 SCRIPTS_TARGET_DIR=~/scripts
 
 this_script=`readlink -f "$0"`
 scripts_src_dir=`dirname "$this_script"`
 dotfile_src_dir=`readlink -f "$scripts_src_dir/../dotfiles"`
 
-echo "syncing ${dotfile_src_dir}/ to ${DOTFILED_TARGET_DIR}/"
-rsync -rt "${dotfile_src_dir}/" "${DOTFILED_TARGET_DIR}/"
+echo "syncing ${dotfile_src_dir}/ to ${DOTFILES_TARGET_DIR}/"
+rsync -rt "${dotfile_src_dir}/" "${DOTFILES_TARGET_DIR}/"
 
 echo "syncing ${scripts_src_dir}/ to ${SCRIPTS_TARGET_DIR}/"
 rsync -rt "${scripts_src_dir}/" "${SCRIPTS_TARGET_DIR}/"
